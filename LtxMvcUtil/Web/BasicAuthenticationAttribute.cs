0dedd62ab7173777cbcb544f135039dddeb29bbd
lattex <jonlimjap@gmail.com>
2014-10-05 23:38:34 +0800

    Adding BasicAuthentication attribute. Enforces requirement for an API Key or similar mechanism

---
+++ LtxMvcUtil/Web/BasicAuthenticationAttribute.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+
+namespace LtxMvcUtil.Web
+{
+    public class BasicAuthenticationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (actionContext.Request.Headers.Authorization == null)
+            {
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            }
+            else
+            {
+                var authToken = actionContext.Request.Headers.Authorization.Parameter;
+                string decodedToken = Encoding.UTF8.GetString(Convert.FromBase64String(authToken));
+
+                string username = decodedToken.Substring(0, decodedToken.IndexOf(":"));
+                string password = decodedToken.Substring(decodedToken.IndexOf(":") + 1);
+
+                if (username == ConfigurationManager.AppSettings["AuthUser"] &&
+                    password == ConfigurationManager.AppSettings["AuthPword"])
+                {
+                    //TODO: Find out how to parameterize this and/or set it outside, or dispose of altogether?
+                    //HttpContext.Current.User = new GenericPrincipal(new ApiIdentity(username), new string[] { });
+
+                    base.OnActionExecuting(actionContext);
+                }
+                else
+                {
+                    actionContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+                }
+            }
+        }
+    }
+}
